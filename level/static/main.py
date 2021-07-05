acfe2dd4ad0bbe10ca6184f2f4328aab7667c571
bw-S0L <78084594+bw-S0L@users.noreply.github.com>
2021-07-06 01:18:37 +0800

    Add files via upload

---
+++ level/static/main.py
@@ -0,0 +1,27 @@
+import flask
+from flask_cors import CORS
+from flask import Flask
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/level1')
+def level1():
+    return flask.send_from_directory('static', 'level1.html')
+
+@app.route('/level2')
+def level2():
+    return flask.send_from_directory('static', 'level2.html')
+
+@app.route('/level3_1')
+def level3_1():
+    return flask.send_from_directory('static', 'level3_1.html')
+@app.route('/level3_2')
+def level3_2():
+    return flask.send_from_directory('static', 'level3_2.html')
+""""@app.route('/favicon.ico')
+def favicon(): 
+    return flask.send_from_directory('static', 'loveheart.ico', mimetype='image/vnd.microsoft.icon')"""
+
+app.run(host='0.0.0.0', port=11667, debug=True)
